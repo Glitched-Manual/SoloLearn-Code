437872de04fcdb9ae238e03beb3b73268fecbfef
Glitched-Manual <popcorneater720@gmail.com>
2017-09-18 18:27:22 -0400

    Update Random_Attack_array.py

--- Random Attack array/Random_Attack_array.py
+++ Random Attack array/Random_Attack_array.py
@@ -8,8 +8,8 @@ arms = ["lazer_gun","deth_ray","null_ray"]
 arms_attack = {"lazer_gun":random.randint(20,40),"deth_ray":random.randint(30,55),
                "null_ray":random.randint(60,90)}
 
+user_input = int(input()) % 3
 
-print(arms_attack[arms[0]])
+print("You selected the "+ str(arms[user_input]) + " you deal " + str(arms_attack[arms[user_input]]) + " to the target!\n")
 
 #buffer output
-input()
